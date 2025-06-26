368e716e17eb32e33bbec21d6001ae23d44f07a4
PhanTruc234 <166579889+PhanTruc234@users.noreply.github.com>
2025-06-26 15:28:30 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
